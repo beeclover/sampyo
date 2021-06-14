28347dc801ae6eb2bcccb189ee8c2516823c0299
Hansanghyeon <999@hyeon.pro>
2021-06-15 04:00:43 +0900

    :sparkles: home - section3 & 4 & 5 & FNB

--- Proposal/B/script.js
+++ Proposal/B/script.js
@@ -119,3 +119,25 @@ class Slideshow {
 }
 
 const slideshow = new Slideshow(document.querySelector(".hero-slide"));
+
+// 인재채용
+const s3_items4 = document.querySelectorAll("[data-scroll-animation-item='4']");
+ScrollTrigger.batch(s3_items4, {
+  onEnter: (batch) => {
+    const tl = gsap.timeline();
+    tl.to(batch, {
+      autoAlpha: 0,
+      y: 40,
+      duration: 0,
+    });
+    tl.to(batch, {
+      autoAlpha: 1,
+      delay: 0.5,
+      stagger: 0.2,
+      y: 0,
+      duration: 0.8,
+    });
+  },
+  onLeaveBack: (batch) => gsap.to(batch, { autoAlpha: 0, duration: 0 }),
+  scrub: true,
+});
