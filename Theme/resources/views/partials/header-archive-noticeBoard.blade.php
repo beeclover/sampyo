23cc9a91aa847cafcf15d5f115becb3fc3a96cae
999hyeon <999@hyeon.pro>
2021-08-26 16:43:03 +0900

    :construction: data matching

--- Theme/resources/views/partials/header-archive-noticeBoard.blade.php
+++ Theme/resources/views/partials/header-archive-noticeBoard.blade.php
@@ -2,14 +2,22 @@
   <header class="header-archive">
     <div class="container lg:max-w-screen-lg mx-auto">
       <div class="header-archive-imgRow">
-        <img
-          src="@php echo get_theme_file_uri('resources/images/header-blog.jpg') @endphp"
-          srcset="
-            @php echo get_theme_file_uri('resources/images/header-blog@2x.jpg') @endphp 2x,
-            @php echo get_theme_file_uri('resources/images/header-blog@3x.jpg') @endphp 3x"
-          alt=""
-          class="header-archive-img"
-        >
+        @if ($thumbnail)
+          <img
+            src="{!! $thumbnail !!}"
+            alt=""
+            class="header-archive-img"
+          >
+        @else
+          <img
+            src="@php echo get_theme_file_uri('resources/images/header-blog.jpg') @endphp"
+            srcset="
+              @php echo get_theme_file_uri('resources/images/header-blog@2x.jpg') @endphp 2x,
+              @php echo get_theme_file_uri('resources/images/header-blog@3x.jpg') @endphp 3x"
+            alt=""
+            class="header-archive-img"
+          >
+        @endif
       </div>
       <div class="header-archive-content">
         <h1 class="header-archive-title">{!! $title !!}</h1>
