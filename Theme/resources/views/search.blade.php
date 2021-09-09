7ad45688813af6dd6b405807d05a52f32fd9e6bf
999hyeon <999@hyeon.pro>
2021-09-10 00:04:22 +0900

    :construction: 검색결과 없을때

--- Theme/resources/views/search.blade.php
+++ Theme/resources/views/search.blade.php
@@ -3,11 +3,6 @@
   <main class="business__page">
     <div class="mb-24"></div>
     <div class="container lg:max-w-screen-lg mx-auto">
-      @if (! have_posts())
-        <x-alert type="warning">
-          {!! __('Sorry, no results were found.', 'sage') !!}
-        </x-alert>
-      @endif
       <div class="bg-atmosphere text-white mb-16">
         {!! get_search_form(false) !!}
       </div>
@@ -16,11 +11,20 @@
         <span class="text-4xl font-black">‘{{ get_search_query() }}’ 검색결과</span> <span class="text-2xl">총 {{ $allCount }}건</span>
       </div>
 
-      <article>
-        @while(have_posts()) @php(the_post())
-          @include('partials.content-search')
-        @endwhile
-      </article>
+      @if (! have_posts())
+        <div class="my-12">
+          <x-alert type="warning">
+            {!! __('Sorry, no results were found.', 'sage') !!}
+          </x-alert>
+        </div>
+      @else
+        <article>
+          @while(have_posts()) @php(the_post())
+            @include('partials.content-search')
+          @endwhile
+        </article>
+      @endif
+
 
       @empty(!$pagination = get_the_posts_pagination( array(
         'mid_size' => 2,
